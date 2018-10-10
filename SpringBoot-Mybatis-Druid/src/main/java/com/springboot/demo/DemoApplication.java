53c87a611d4b53ea928a190d7a2ff7bea6bf3d9c
luowuji <931973052@qq.com>
2018-10-10 15:32:00 +0800

    springboot-mybatis-druid 初始化

--- SpringBoot-Mybatis-Druid/src/main/java/com/springboot/demo/DemoApplication.java
+++ SpringBoot-Mybatis-Druid/src/main/java/com/springboot/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(DemoApplication.class);
+
         app.run(args);
     }
 }
