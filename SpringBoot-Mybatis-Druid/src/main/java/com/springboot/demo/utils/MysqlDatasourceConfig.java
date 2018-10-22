8cd00fae9ec5b0f7be7c1d2c1c3ab980a12c2656
luowuji <931973052@qq.com>
2018-10-22 17:45:03 +0800

    配置mysql-druid数据源

---
+++ SpringBoot-Mybatis-Druid/src/main/java/com/springboot/demo/utils/MysqlDatasourceConfig.java
@@ -0,0 +1,54 @@
+package com.springboot.demo.utils;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = MysqlDatasourceConfig.PACKAGE,sqlSessionFactoryRef = "mysqlSqlsessionFactory")
+public class MysqlDatasourceConfig {
+
+    // MySQL DAO扫描目录
+    static final String PACKAGE = "com.springboot.demo.service.*.dao";
+    // mybatis mapper扫描路径
+    static final String MAPPER_LOCATION = "classpath*:com/springboot/demo/service/**/*Mapper.xml";
+
+
+    @Primary
+    @Bean(name = "mysqldatasource")
+    @ConfigurationProperties("spring.datasource.druid.mysql")
+    public DataSource mysqlDataSource(){
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "mysqlTransactionManager")
+    @Primary
+    public DataSourceTransactionManager mysqlTransactionManager(){
+        return new DataSourceTransactionManager(mysqlDataSource());
+    }
+
+    @Bean(name = "mysqlSqlSessionFactory")
+    @Primary
+    public SqlSessionFactory mysqlSqlSessionFactory(@Qualifier("mysqldatasource")DataSource dataSource) throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        // xml方式配置mapper地址，如不使用xml方式可以省略下面这行配置
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(MysqlDatasourceConfig.MAPPER_LOCATION));
+        return sessionFactory.getObject();
+    }
+
+
+
+
+
+}
